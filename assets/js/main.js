e1cd8658e610101361717936857571689187ade7
waqasnoor222 <waqasnoor222>
2024-08-29 19:34:32 +0500

    initial commit packages page done

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$('#gallery').slick({
+  infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    draggable: true,
+    speed: 300,
+    autoplaySpeed: 1000,
+    arrows: false
+  });
