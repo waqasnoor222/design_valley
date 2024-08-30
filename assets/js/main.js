2549747ce5fff3137095b85d5a17c2501001bfc1
waqasnoor222 <waqasnoor222>
2024-08-30 15:17:39 +0500

    working perfect

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,20 @@ $('#gallery').slick({
   infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     draggable: true,
     speed: 300,
     autoplaySpeed: 1000,
     arrows: false
   });
+
+  $('.port_slider').slick({
+    infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      draggable: true,
+      speed: 300,
+      autoplaySpeed: 1000,
+      arrows: false
+    });
